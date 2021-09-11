2eacc608433898fa5fe79be81c92632eb4d270b9
Szymon Rams <szymon.rams@icloud.com>
2021-09-11 15:28:15 +0200

    change

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const os = require('os');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+host = os.hostname();
+
+app.get('/', (req, res) => res.send(`Hello World! This should run on ${host}`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
